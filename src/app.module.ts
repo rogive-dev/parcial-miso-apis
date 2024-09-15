6af906f8106f80c29b170fe357f58aee34adfd1e
Natalia Rodriguez <ierodriguezgo@gmail.com>
2024-09-14 21:36:06 -0500

    Setup TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,33 @@ import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
 import { SupermercadoModule } from './supermercado/supermercado.module';
 import { CiudadSupermercadoModule } from './ciudad-supermercado/ciudad-supermercado.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadEntity } from './ciudad/ciudad.entity/ciudad.entity';
+import { SupermercadoEntity } from './supermercado/supermercado.entity/supermercado.entity';
 
 @Module({
-  imports: [CiudadModule, SupermercadoModule, CiudadSupermercadoModule],
+  imports: [
+    CiudadModule,
+    SupermercadoModule,
+    CiudadSupermercadoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'supermarkets',
+      entities: [
+        CiudadEntity,
+        SupermercadoEntity,
+      ],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
