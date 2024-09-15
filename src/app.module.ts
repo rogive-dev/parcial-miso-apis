30799a5d03bb252685fef8e43276741683207dd6
Natalia Rodriguez <ierodriguezgo@gmail.com>
2024-09-14 19:00:36 -0500

    Create modules for ciudad, supermercado, and the relationship between ciudad and supermercado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CiudadModule } from './ciudad/ciudad.module';
+import { SupermercadoModule } from './supermercado/supermercado.module';
+import { CiudadSupermercadoModule } from './ciudad-supermercado/ciudad-supermercado.module';
 
 @Module({
-  imports: [],
+  imports: [CiudadModule, SupermercadoModule, CiudadSupermercadoModule],
   controllers: [AppController],
   providers: [AppService],
 })
